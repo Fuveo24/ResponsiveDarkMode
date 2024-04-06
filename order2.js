09a4a6246c2ebc67d8a8f71824bc8e1e68bc5687
Fuveo24 <151065648+Fuveo24@users.noreply.github.com>
2024-04-06 15:38:45 +0200

    Add files via upload

---
+++ order2.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Voice AI',
+        productNumber: '85743',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'Image AI',
+        productNumber: '97245',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Code AI',
+        productNumber: '36452',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
